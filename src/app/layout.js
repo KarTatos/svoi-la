d2f98f0e1e129b44e7f7b7424d716d22d1128a0b
KarTatos <kushnir4work@gmail.com>
2026-04-03 20:05:15 -0700

    photo and zoom fixes

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,9 @@ export const viewport = {
   width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
+  minimumScale: 1,
   userScalable: false,
+  viewportFit: 'cover',
 };
 
 export default function RootLayout({ children }) {
@@ -21,7 +23,7 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="МЫ в LA" />
         <link rel="apple-touch-icon" href="/icon-192.png" />
       </head>
